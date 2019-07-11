edcfc8393bf9f0810c084382a0e1f34ddf694fba
makefu <github@syntax-fehler.de>
2019-07-11 08:02:47 +0200

    default.nix: also distribute json files

--- default.nix
+++ default.nix
@@ -3,6 +3,10 @@ rec {
   muellshack = mkYarnPackage {
     name = "muellshack";
     src = ./.;
+    postInstall = ''
+      install -D -t $out/share static_muelldata.json
+      install -D -t $out/share storage.json
+    '';
     packageJSON = ./package.json;
     yarnLock = ./yarn.lock;
     yarnNix = ./yarn.nix;
